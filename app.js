f2df05c5ebd400e9f5d026029715246204e5b4ef
Radheshyam <142769686+radheshyamgupta01@users.noreply.github.com>
2023-11-15 00:07:12 +0530

    added express js backend-part simple group-chat

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+// server/app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors middleware
+const fs = require('fs');
 
-const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors()); // Enable CORS for all routes
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>homepage</h1>");
+app.get('/', (req, res) => {
+  res.send('Welcome to the chat app backend!');
 });
 
-const adminRoute = require("./Routes/admin");
-app.use("/ram", adminRoute);
- const shopRout=require("./Routes/shop")
- app.use("/shop",shopRout)
-
-app.use((req, res) => {
-  res.status(404).send("<h1>page not found</h1>");
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  res.json({ success: true, username });
 });
 
-app.listen(9090, () => {
-  console.log(`Running on port 9090`);
+app.post('/send-message', (req, res) => {
+  try {
+    const { username, message } = req.body;
+    console.log(username,message)
+
+    // Read existing messages
+    // const messages = JSON.parse(fs.readFileSync('messages.json', 'utf8')) || [];
+
+    // // Add the new message
+    // messages.push({ username, message });
+
+    // // Write updated messages back to the file
+    // fs.writeFileSync('messages.json', JSON.stringify(messages), 'utf8');
+
+    // Respond with success
+    res.json({ success: true, message, username });
+  } catch (error) {
+    // Handle errors and respond with an error status
+    console.error('Error:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
