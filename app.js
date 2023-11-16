660aa12a65ba65231caf4d72be7bf637423fc7c9
Radheshyam <142769686+radheshyamgupta01@users.noreply.github.com>
2023-11-16 06:49:03 +0530

    data stored in file

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// server/app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import cors middleware
-const fs = require('fs');
 
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the chat app backend!');
+  res.send(`
+    <html>
+      <body>
+        <h1>Welcome to the backend app</h1>
+        <button><a href="/contact">Contact</a></button>
+      </body>
+    </html>
+  `);
 });
 
 app.post('/login', (req, res) => {
@@ -19,30 +29,77 @@ app.post('/login', (req, res) => {
   res.json({ success: true, username });
 });
 
-app.post('/send-message', (req, res) => {
+function readAndWriteToFile(filePath, data) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, fileData) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const newData = JSON.parse(fileData || '[]');
+      newData.push(data);
+
+      fs.writeFile(filePath, JSON.stringify(newData), 'utf8', (writeErr) => {
+        if (writeErr) {
+          reject(writeErr);
+          return;
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+app.post('/send-message', async (req, res) => {
   try {
-    const { username, message } = req.body;
-    console.log(username,message)
+    const { username, message,password } = req.body;
+    console.log(username, message,password);
 
-    // Read existing messages
-    // const messages = JSON.parse(fs.readFileSync('messages.json', 'utf8')) || [];
+    // Specify the path to your data file
+    const dataFilePath = path.join(__dirname, 'data.json');
 
-    // // Add the new message
-    // messages.push({ username, message });
+    // Use the function to read and write data to the file
+    await readAndWriteToFile(dataFilePath, { username, message,password });
 
-    // // Write updated messages back to the file
-    // fs.writeFileSync('messages.json', JSON.stringify(messages), 'utf8');
-
-    // Respond with success
-    res.json({ success: true, message, username });
+    res.json({ success: true, message, username,password });
   } catch (error) {
-    // Handle errors and respond with an error status
     console.error('Error:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
+app.get('/contact', (req, res) => {
+  const contactFile = path.join(__dirname, 'Views', 'contact.html');
+  res.sendFile(contactFile);
+});
 
+app.post('/success', async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password= req.body.password;
+    console.log(name, email,password);
+
+    // Specify the path to your data file
+    const dataFilePath = path.join(__dirname, 'data.json');
+
+    // Use the function to read and write data to the file
+    await readAndWriteToFile(dataFilePath, { name, email ,password});
+
+    res.send(`
+      <html>
+        <body>
+          <h1>Message sent successfully</h1>
+          <button><a href="/">Back to Home</a></button>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
